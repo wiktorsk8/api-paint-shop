e92a11a84657b8df99c2eac647f20daeca61b62e
wiktorsk8 <wikto4k@gmail.com>
2023-07-06 15:54:00 +0200

    user crud permissions added and register form refactored

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+
+class UserPolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+        
+    }
+
+    public function viewAny(User $user): bool{
+        return $user->is_admin;
+    }
+
+    public function view(User $user): bool{
+        return $user->is_admin;
+    }
+
+    public function update(User $user, User $userToUpdate): bool{
+        return $user->is_admin;
+    }
+
+    public function delete(User $user, User $userToDelete): bool{
+        if ($user->is_admin) return true;
+
+        if ($user->id == $userToDelete->id){
+            return true;
+        }
+
+        return false;
+    }
+}
