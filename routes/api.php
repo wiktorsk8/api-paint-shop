2ffa73108c41467a43e7b85a60640b28fc764eaa
wiktorsk8 <wikto4k@gmail.com>
2023-07-28 16:48:32 +0200

    Namespace fix

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
